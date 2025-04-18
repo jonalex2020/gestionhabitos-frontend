672023b896ec99746c81616316d28c7b937681b3
jonnymerida <jonny.merida@cicrl.com.gt>
2025-04-17 19:47:19 -0600

    feat/cambios para que funcione las paginas

---
+++ app/login/page.tsx
@@ -0,0 +1,7 @@
+const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email, password }),
+  });
+  
